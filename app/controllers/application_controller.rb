e04c2492d14f9e517a24508ca90abb526614f0a4
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-02 10:27:29 -0500

    post via class.create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
-  
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
   end
-  
+
   get '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
@@ -22,5 +22,22 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery_to_update = Bakery.find_by(params[:id])
+    bakery_to_update.update(name: params[:name])
+    bakery_to_update.to_json
+  end
+
+  
 
 end
